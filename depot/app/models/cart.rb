8a0e2a68d2c24ac25299d64d692846d8d37e1476
Malcontents <tristan.rose@hotmail.com>
2017-01-25 19:28:16 -0500

    Prettier Cart Layout

--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -1,5 +1,9 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end
 
 def add_product(product)
@@ -10,8 +14,4 @@ else
 current_item = line_items.build(product_id: product.id)
 end
 current_item
-end
-
-def total_price
-  line_items.to_a.sum { |item| item.total_price }
 end
